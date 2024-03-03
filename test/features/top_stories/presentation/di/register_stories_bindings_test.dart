ec6d9a3620de424db16cb7eca93720e5c75b193e
ahmad daly <Ahmaddo1999@gmail.com>
2024-03-03 16:06:31 +0400

    created unit test for the top_stories feature dependency injector

---
+++ test/features/top_stories/presentation/di/register_stories_bindings_test.dart
@@ -0,0 +1,25 @@
+import 'package:bab_news/core/di/dependency_injector.dart';
+import 'package:bab_news/features/top_stories/domain/use_cases/launch_url_use_case.dart';
+import 'package:bab_news/features/top_stories/domain/use_cases/navigate_details_use_case.dart';
+import 'package:bab_news/features/top_stories/presentation/ui/provider/stories_data_provider.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+
+void main() async {
+  group('test network dependency injector', () {
+    test('test that [ApiRequestsRepositoryImp] singleton is registered',
+        () async {
+      // Arrange
+      final GetIt getIt = GetIt.instance;
+
+      // Act
+      await DependencyInjector.init(getIt);
+
+      // assert
+      expect(getIt<StoriesDataProvider>(), isA<StoriesDataProvider>());
+      expect(getIt<NavigateDetailsUseCase>(), isA<NavigateDetailsUseCase>());
+      expect(getIt<LaunchUrlUseCase>(), isA<LaunchUrlUseCase>());
+    });
+  });
+}
